8111e24d092358ee22947de89ccdee3e94ba84a8
eduarda.santos <eduarda.santos@PED-SKA>
2022-01-30 10:45:15 -0300

    Aula 3: enum

--- coffee-manager/src/app/components/main-coffee/main-coffee.component.ts
+++ coffee-manager/src/app/components/main-coffee/main-coffee.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MainCoffeeService } from '../../services/main-coffee.service';
+import { Events_e } from '../../../../../common/enum';
 
 @Component({
   selector: 'app-main-coffee',
@@ -11,12 +12,20 @@ export class MainCoffeeComponent implements OnInit {
   @Input() auxData: string;
   @Output() initMainCoffee = new EventEmitter();
 
+  status: number;
+
   constructor(private mainCoffeeService: MainCoffeeService) { }
 
   ngOnInit(): void {
-    console.log(this.mainCoffeeService.coffeeMakerName);
-    console.log('Input auxData: ', this.auxData);
-    this.initMainCoffee.emit(true);
+    // Evitar as comparações com string
+    // if (this.status == 'Produção') {
+      // Algo acontece
+    // }
+
+    // Mas também deixar claro o que o status representa
+    if (this.status == Events_e.Producao) {
+      // Isso seria igual a this.status == 0
+    }
   }
 
 }
