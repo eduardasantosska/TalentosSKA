356ed37fce9a8d759e7fb0b6df90e9d563f92bb7
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-04 11:01:08 -0300

    Aula 3: tipagem dinamica

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,12 +1,6 @@
 inicio();
 
 function inicio() {
-    // falta inicialização 
-    const escopoBloco; 
-    
-    // erro ao tentar alterar valor 
-    // const escopoBloco = 'Primeiro valor'; 
-    // console.log(escopoBloco); 
-    // escopoBloco = 'Segundo valor'; 
-    // console.log(escopoBloco); 
+    let tipoDinamico = 4;
+    tipoDinamico = "Mudando para string";
 }
