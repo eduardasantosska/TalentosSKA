27cb090db582fd941e05b3e5032c2fbe46c1ee56
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-04 11:34:04 -0300

    Aula 3: Number - valores especiais

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,4 +1,11 @@
-const num = 1234.56;
-console.log("Valor inteiro: ", Number.parseInt(num));
-console.log("Valor ao quadrado: ", Math.pow(num, 2));
+const numNaN = Number('Teste');
+console.log("numNaN: ", numNaN);
+console.log("é NaN: ", isNaN(numNaN));
 
+const numInfinity = 6/0;
+console.log("\nnumInfinity: ", numInfinity);
+console.log("é Infinity: ", numInfinity == Infinity);
+
+console.log("\nisInfinity nunNaN: ", isFinite(numNaN));
+console.log("isInfinity numInfinity: ", isFinite(numInfinity));
+console.log("isInfinity válido: ", isFinite(4));
