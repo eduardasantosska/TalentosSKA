0068ac85a79442ac8ae2a308fc8a84ccfdc10192
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-04 10:37:46 -0300

    Aula 3: Let

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,9 +1,16 @@
 inicio();
 
 function inicio() {
-    var nome;
-    console.log("Variável nome: ", nome);
+    if (true) {
+        var escopoFuncao;
+        let escopoBloco;
 
-    nome = "Eduarda";
-    console.log("Atualização de valor: ", nome);
+        escopoFuncao = "VAR";
+        escopoBloco = "LET";
+
+        console.log("IF escopoFuncao: ", escopoFuncao); // VAR
+        console.log("IF escopoBloco: ", escopoBloco); // LET 
+    }
+    console.log("escopoFuncao: ", escopoFuncao); // VAR
+    console.log("escopoBloco: ", escopoBloco); // LET 
 }
