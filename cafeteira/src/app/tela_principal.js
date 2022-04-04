dbab40e6fa1ef06098b962b71615a6de9ee8f45c
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-04 13:24:09 -0300

    Aula 3 - Strings - expressões

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,17 +1,4 @@
-let animal = '   gato';
-console.log('Original:', animal);
+const num1 = 1;
+const num2 = 2;
 
-// concatenar
-animal = animal + ' e cachorro   ';
-console.log('Concatenar:', animal);
-// alterar os caracteres
-animal = animal.toUpperCase();
-console.log('Alterar caracteres:', animal);
-// remover em branco
-animal = animal.trim();
-console.log('Remover em branco:', animal);
-// verificar se faz parte
-console.log('Verificar se gato faz parte:', animal.includes('GATO'));
-// remover
-animal = animal.slice(0,4);
-console.log('Remover:', animal);
+console.log(`Somando ${num1} com ${num2} teremos ${num1 + num2}`);
