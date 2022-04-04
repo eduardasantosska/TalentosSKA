02065047814d014af972403bfb9c192d093ec71b
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-04 11:21:52 -0300

    Aula 3: objetos Number e Math

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,2 +1,4 @@
-const num = 1_000_000;
-console.log("Num: ", num);
+const num = 1234.56;
+console.log("Valor inteiro: ", Number.parseInt(num));
+console.log("Valor ao quadrado: ", Math.pow(num, 2));
+
