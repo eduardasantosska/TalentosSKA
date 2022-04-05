400b896c5935f2e9823a42851c210cf6a128e7cc
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-05 08:32:46 -0300

    Aula 3 - Array - exemplos

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,10 +1,6 @@
 const nomes = new Array();
 nomes.push("Amanda", "Talita", "Marcele", "Larissa");
 
-// Para acessar uma posição especifica do array
-// array[posição]
-console.log("Posição 0: ", nomes[0]);
-
 // Para excluir um registro
 // splice(posição, qtd itens)
 nomes.splice(1, 1);
@@ -12,3 +8,10 @@ console.log("Nomes: ", nomes);
 
 // Para verificar se é um array
 console.log("É um array? ", Array.isArray(nomes));
+
+// Quantidade de itens
+console.log("Quantidade de itens: ", nomes.length);
+
+// Para acessar uma posição especifica do array
+// array[posição]
+console.log("Posição 0: ", nomes[0]);
