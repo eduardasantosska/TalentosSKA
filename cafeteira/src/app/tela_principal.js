9a890a6a348e056526cc4a720494decf00ac3046
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-05 08:17:44 -0300

    Aula 3 - Array - excluir itens

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -2,4 +2,10 @@ const nomes = new Array();
 nomes.push("Amanda", "Talita", "Marcele", "Larissa");
 
 // Para acessar uma posição especifica do array
+// array[posição]
 console.log("Posição 0: ", nomes[0]);
+
+// Para excluir um registro
+// splice(posição, qtd itens)
+nomes.splice(1, 1);
+console.log("Nomes: ", nomes);
