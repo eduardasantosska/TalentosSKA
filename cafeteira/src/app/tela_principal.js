a745f3737a3a90c169156cc488040c23cd0e95d7
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-05 08:43:55 -0300

    Aula 3 - Array - métodos

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,17 +1,8 @@
 const nomes = new Array();
 nomes.push("Amanda", "Talita", "Marcele", "Larissa");
 
-// Para excluir um registro
-// splice(posição, qtd itens)
-nomes.splice(1, 1);
-console.log("Nomes: ", nomes);
+// Includes(): verifica se um array contem um valor
+console.log("Nomes possui Talita: ", nomes.includes("Talita"));
 
-// Para verificar se é um array
-console.log("É um array? ", Array.isArray(nomes));
-
-// Quantidade de itens
-console.log("Quantidade de itens: ", nomes.length);
-
-// Para acessar uma posição especifica do array
-// array[posição]
-console.log("Posição 0: ", nomes[0]);
+// IndexOf(): acha o index de um valor
+console.log("Index de Talita: ", nomes.indexOf("Talita"));
