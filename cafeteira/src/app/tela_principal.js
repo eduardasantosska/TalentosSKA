40fb87dfa7269a42f0eb3f1fdba3ab8b855e8403
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-05 09:48:19 -0300

    Aula 3 - Null e Undefined

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,23 +1,6 @@
-const objAluno = {
-    nome: "Camila"
-    , idade: 16
-    , telefone: 51985456889
-}
+let nome;
+console.log("Nome declarado sem valor: ", nome);
 
-// Recuperar valor do objeto
-const pNome = "nome";
-const pIdade = "idade";
-
-console.log("1: ", objAluno.nome);
-console.log("2: ", objAluno["nome"]);
-console.log("3: ", objAluno[pNome]);
-
-// Adicionar ou atualizar valor
-objAluno.idade = 17;
-objAluno["idade"] = 17;
-objAluno[pIdade] = 17;
-console.log("Idade atualizada: ", objAluno);
-
-// Apagar uma propriedade
-delete objAluno.telefone
-console.log("Remover telefone: ", objAluno);
+nome = null;
+console.log("Nome é null? ", nome===null);
+console.log("Testando com typeof - erro: ", typeof nome);
