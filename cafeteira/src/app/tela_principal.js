57e9d2637bd95195efe3e02e38e89032b1557ab3
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-08 12:12:35 -0300

    Aula 4: uso de JS com HTML

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,6 +1,1 @@
-let numeros = [1, 2, 3];
-
-let numerosMaisUm = numeros.map((valor1) => valor+1);
-
-const soma2 = (valor1, valor2) => valor1+valor2;
-
+document.getElementById("teste").innerText = "Teste - tela principal" 
