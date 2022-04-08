b27af80ba5ade73a65cadebd38af675c941139eb
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-08 11:40:08 -0300

    Aula 4: arrow function e funções array da última aula

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,11 +1,6 @@
-const soma = (valor1, valor2) => {
-    return valor1+valor2;
-}
+let numeros = [1, 2, 3];
+
+let numerosMaisUm = numeros.map((valor1) => valor+1);
 
 const soma2 = (valor1, valor2) => valor1+valor2;
 
-v1 = 1;
-v2 = 3;
-valorSomado = soma2(v1, v2);
-
-console.log(`Soma dos valores ${v1} e ${v2} = ${valorSomado}`);
