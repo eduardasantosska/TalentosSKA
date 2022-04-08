ab985acb51c61e0b5fb6cbba945c46503d252fbc
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-08 10:00:22 -0300

    Aula 3: for interrupção

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,5 +1,19 @@
 const alunas = ["Ana", "Bruna", "Carla"];
 
+// CONTINUE
+// se não quiser imprimir alunas que começam com B
+for(let i=0; i<alunas.length; i++) {
+    if(alunas[i].startsWith("B")) {
+        continue;
+    }
+    console.log(alunas[i]);
+}
+
+// BREAK
+// se quer percorrer até achar uma aluna em especifico
 for(let i=0; i<alunas.length; i++) {
     console.log(alunas[i]);
+    if(alunas[i] == "Bruna") {
+        break;
+    }
 }
