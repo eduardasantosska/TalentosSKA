83ebdc31abe9cc44d48cf4a5a167b2a7c0726e21
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-08 11:32:00 -0300

    Aula 4: return implicito e explicito

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,13 +1,11 @@
-// function soma(valor1, valor2) {
-//     return valor1+valor2;
-// }
-
 const soma = (valor1, valor2) => {
     return valor1+valor2;
 }
 
+const soma2 = (valor1, valor2) => valor1+valor2;
+
 v1 = 1;
 v2 = 3;
-valorSomado = soma(v1, v2);
+valorSomado = soma2(v1, v2);
 
 console.log(`Soma dos valores ${v1} e ${v2} = ${valorSomado}`);
