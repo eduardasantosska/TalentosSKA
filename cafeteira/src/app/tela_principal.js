87450cf2b9beb33fd963eace334629954764a169
EduardaVargasdosSantos <eduarda.santos@ska.com.br>
2022-04-08 10:15:31 -0300

    Aula 4: loop while

--- cafeteira/src/app/tela_principal.js
+++ cafeteira/src/app/tela_principal.js
@@ -1,19 +1,8 @@
 const alunas = ["Ana", "Bruna", "Carla"];
 
-// CONTINUE
-// se não quiser imprimir alunas que começam com B
-for(let i=0; i<alunas.length; i++) {
-    if(alunas[i].startsWith("B")) {
-        continue;
-    }
+let i=0;
+while(i<alunas.length) {
     console.log(alunas[i]);
+    i++;
 }
 
-// BREAK
-// se quer percorrer até achar uma aluna em especifico
-for(let i=0; i<alunas.length; i++) {
-    console.log(alunas[i]);
-    if(alunas[i] == "Bruna") {
-        break;
-    }
-}
